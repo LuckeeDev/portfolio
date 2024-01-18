c5589ef21315aa2622b156c1bfa1ebb910427368
Luca Zoppetti <lucazoppetti@outlook.com>
2024-01-18 22:46:56 +0100

    convert presentation to json file

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,33 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const postsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    publishedAt: z.date(),
-    description: z.string(),
-    isPublish: z.boolean(),
-    isDraft: z.boolean().default(false),
-  }),
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		publishedAt: z.date(),
+		description: z.string(),
+		isPublish: z.boolean(),
+		isDraft: z.boolean().default(false),
+	}),
 });
 
-export const collections = { posts: postsCollection };
+const presentationCollection = defineCollection({
+	type: 'data',
+	schema: z.object({
+		email: z.string().email(),
+		title: z.string(),
+		description: z.string(),
+		profile: z.string(),
+		socials: z.array(
+			z.object({
+				link: z.string().url(),
+				label: z.string(),
+			})
+		),
+	}),
+});
+
+export const collections = {
+	posts: postsCollection,
+	presentation: presentationCollection,
+};
