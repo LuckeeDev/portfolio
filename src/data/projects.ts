bf9d680a30beb99d93525cee5b9bc4595ab5b5e6
Luca Zoppetti <lucazoppetti@outlook.com>
2024-01-31 23:41:25 +0100

    fix deploy errors

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,16 @@
 export type Project = {
-  title: string;
-  techs: string[];
-  link: string;
-  isComingSoon?: boolean;
+	title: string;
+	techs: string[];
+	link: string;
+	isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
-  {
-    title: "Dictionary App",
-    techs: ["ReactJS (NextJS)", "react-query", "zod"],
-    link: "https://github.com/MaeWolff/dictionary-app",
-  },
-  {
-    title: "Portfolio / Lina BLIDI",
-    techs: ["ReactJS (NextJS)", "TypeScript"],
-    link: "https://www.linablidi.fr/",
-  },
-  {
-    title: "Portfolio / Template",
-    techs: ["Astro"],
-    link: "/",
-    isComingSoon: true,
-  },
+	{
+		title: 'LuTeX',
+		techs: ['LaTeX'],
+		link: 'https://tex.lucazoppetti.me/',
+	},
 ];
 
 export default projects;
